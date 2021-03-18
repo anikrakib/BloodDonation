3bd704474cd6fc3bf5e715e794d7e50fa9775159
anikrakib <anikrakib1998@gmail.com>
2021-03-18 18:25:16 +0600

    create custom style for button and some changes in welcome activity

--- app/src/main/java/com/anikrakib/blooddonation/Activity/WelComeActivity.java
+++ app/src/main/java/com/anikrakib/blooddonation/Activity/WelComeActivity.java
@@ -2,15 +2,24 @@ package com.anikrakib.blooddonation.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.anikrakib.blooddonation.R;
+import com.anikrakib.blooddonation.databinding.ActivityWelcomeBinding;
 
 public class WelComeActivity extends AppCompatActivity {
+    ActivityWelcomeBinding activityWelcomeBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        activityWelcomeBinding = ActivityWelcomeBinding.inflate(getLayoutInflater());
+        setContentView(activityWelcomeBinding.getRoot());
+
+        activityWelcomeBinding.startJourneyButton.setOnClickListener(v->{
+            startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
+            finish();
+        });
     }
 }
